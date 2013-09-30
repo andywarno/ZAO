d4002628077f11e6e5fd96f94fb7a2f2dd98a7c6
Andrew Woroniecki <andywarno@gmail.com>
2013-09-30 16:40:38 -0400

    Added DataInsert.cs. Not yet utilized in the program.

---
+++ MainBranch/SeniorDesign/SeniorDesign/DataInsert.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace SeniorDesign
+{
+    class DataInsert
+    {
+        public static void ImportData()
+        {
+            MySqlConnection con = DBConnection.conn;
+            List<string[]> rows = new List<string[]>();
+            try
+            {
+                
+                string[] temp = {"..//../..//tableScript.sql"};
+                string command = "";
+                foreach (string s in temp)
+                {
+                    using (StreamReader sr = new StreamReader(s))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            if (line.Contains(";"))
+                            {
+                                MySqlCommand cmd = new MySqlCommand(line, con);
+                                cmd.ExecuteNonQuery();
+                                command = "";
+                            }
+                            else
+                            {
+                                command += line;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error: {0}", ex));
+            }
+        }
+    }
+}
+
+
